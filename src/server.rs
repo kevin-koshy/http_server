d7aa083de113e5e0f069cbe5fbb840eb7d6312c7
Kevin Koshy <kevin_koshy@hotmail.com>
2022-07-01 16:48:15 +0530

    General cleanup

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     address: String,
 }
@@ -9,5 +12,25 @@ impl Server {
 
     pub fn run(self) {
         println!("Server started on {}", self.address);
+
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop {
+
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer= [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e)=> println!("Failed to read from Connection"),
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+
+        }
+
     }
 }
