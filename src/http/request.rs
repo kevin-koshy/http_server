67549085614c381c08c9aea6f209163ca093dd47
Kevin Koshy <kevin_koshy@hotmail.com>
2022-07-05 12:43:47 +0530

    General functionality 1

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,60 @@
-use super::Method;
+use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::fmt::Result as FmtResult;
+use std::fmt::Debug;
+use std::str;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        str::from_utf8(buf);
+        unimplemented!()
+    }
+}
+
+pub enum ParseError{
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod
+}
+
+
+impl ParseError{
+    fn message(&self) -> &str {
+        match self{
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+
+        }
+    }
+}
+
+impl Display for ParseError{
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError{
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+
+impl  Error for ParseError {
+
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::io::Read;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::net::TcpListener;
+use crate::http::Request;
 
 pub struct Server {
     address: String,
@@ -23,6 +26,11 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer)[..]{
+                                Ok(request) => {},
+                                Err(e) => println!{"Failed with an error {}", e}
+                            }
+
                         },
                         Err(e)=> println!("Failed to read from Connection {}", e),
                     }
